64847ffc3ec9c811b4b489dae4f4d4f74414ab4c
tuandui <tuandui@tsign.cn>
2025-04-11 17:08:18 +0800

    feat: eslint配置文件重构

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,51 +1,46 @@
 import { defineConfig } from 'eslint/config'
 import globals from 'globals'
+import pluginJs from '@eslint/js'
 import { configs, parser } from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
-import js from '@eslint/js'
-import eslintPluginImportX from 'eslint-plugin-import-x'
-import tsParser from '@typescript-eslint/parser'
-// 导入这个可以一次性设置 `eslint-plugin-prettier` 和 `eslint-config-prettier`
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintPluginImportX from 'eslint-plugin-import-x'
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-    ignores: ['eslint.config.mjs'],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module'
-    },
-    rules: {
-      'no-unused-vars': 'off',
-      'import-x/no-dynamic-require': 'warn',
-      'import-x/no-nodejs-modules': 'warn'
-    }
-  },
-
-  ...configs.recommended,
-  eslintPluginImportX.flatConfigs.recommended,
-  eslintPluginImportX.flatConfigs.typescript,
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,vue}'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node, wx: true, uni: true } }
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,vue}'],
-    plugins: { js },
-    extends: ['js/recommended']
-  },
-
-  pluginVue.configs['flat/essential'],
-  {
-    files: ['**/*.vue'],
-
-    languageOptions: { parserOptions: { parser: parser } }
-  },
-  {
     ignores: ['node_modules', 'src/static/', '.vscode', '.husky']
   },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,,ts,tsx,vue}'],
+    ignores: ['eslint.config.mjs'],
+    languageOptions: {
+      parser: parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    }
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        wx: true,
+        uni: true,
+        WechatMiniprogram: true,
+        getCurrentPages: true,
+        UniHelper: true
+      }
+    }
+  },
+  // @eslint/js 扩展插件
+  pluginJs.configs.recommended,
+  // typescript-eslint 扩展插件
+  ...configs.recommended,
+  // eslint-plugin-vue 扩展插件
+  ...pluginVue.configs['flat/essential'],
+  // eslint-plugin-vue 配置对象属性
+  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: parser } } },
+  eslintPluginImportX.flatConfigs.recommended,
+  eslintPluginImportX.flatConfigs.typescript,
   eslintPluginPrettierRecommended
 ])
